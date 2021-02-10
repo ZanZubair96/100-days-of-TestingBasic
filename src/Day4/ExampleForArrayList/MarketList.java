f8efde71a09c6b7240ef47383c7e22138d1a9b4c
ZanZubair96 <zanzubair2@gmail.com>
2021-02-11 01:39:51 +0530

    1.Day-4 ArrayList Code Commit.

--- src/Day4/ExampleForArrayList/MarketList.java
+++ src/Day4/ExampleForArrayList/MarketList.java
@@ -1,16 +1,40 @@
 package Day4.ExampleForArrayList;
 
 
-/**Size is dynamic
-        Only supports objects
-        not syncronized
-        array based class
+import java.util.ArrayList;
 
-ArrayList is array based, internally uses array
-                LinkedList consists of nodes/values that are related to each other
-        ArrayList and LinkedList both keep ordering
-        ArrayList and LinkedList both allow duplicates
+/**
+ * Size is dynamic
+ * Only supports objects
+ * not syncronized
+ * array based class
+ * <p>
+ * ArrayList is array based, internally uses array
+ * LinkedList consists of nodes/values that are related to each other
+ * ArrayList and LinkedList both keep ordering
+ * ArrayList and LinkedList both allow duplicates
  */
 
 public class MarketList {
+    public static void main(String[] args) {
+        ArrayList<String> listForMarket = new ArrayList<>();
+        listForMarket.ensureCapacity(2);
+
+
+        listForMarket.add("Banana");
+        listForMarket.add("Sugar");
+        listForMarket.add("Maggi");
+        listForMarket.add("Chocolates");
+        listForMarket.add("Sugar");
+
+        System.out.println("Did you add Maggi:  "+listForMarket.contains("Maggi"));
+        System.out.println(listForMarket);
+
+
+        System.out.println("What you entered in the List at 2: " + listForMarket.get(1));
+        System.out.println(listForMarket.remove("Sugar"));
+
+        System.out.println(listForMarket);
+
+    }
 }
