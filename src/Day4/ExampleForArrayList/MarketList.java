5368b23c7893cb666270920fa82b365975c59eec
ZanZubair96 <zanzubair2@gmail.com>
2021-02-11 01:22:34 +0530

    1.Day-4 ArrayList Explanation.

---
+++ src/Day4/ExampleForArrayList/MarketList.java
@@ -0,0 +1,16 @@
+package Day4.ExampleForArrayList;
+
+
+/**Size is dynamic
+        Only supports objects
+        not syncronized
+        array based class
+
+ArrayList is array based, internally uses array
+                LinkedList consists of nodes/values that are related to each other
+        ArrayList and LinkedList both keep ordering
+        ArrayList and LinkedList both allow duplicates
+ */
+
+public class MarketList {
+}
